5a6b42b31c225ba950b61ac6933cca1c6fd1587f
mateusz.kasprowicz <mateusz.kasprowicz@allegro.com>
2024-04-01 15:44:56 +0200

    Add constants.py

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+RANDOM_STATE = 0
+
+LGBM_MODEL_PARAMS = {
+    "objective": "regression",
+    "metric": "rmse",
+    "num_leaves": 30,
+    "learning_rate": 0.1,
+    "verbosity": -1,
+    "random_state": RANDOM_STATE
+}
